582d335ad29466b9fdd41b4ea90e80837b1fbcd2
denis256 <denis.o@linux.com>
2022-11-06 21:36:33 +0200

    Updated env variables

--- src/main/java/com/timezone/fetcher/Main.java
+++ src/main/java/com/timezone/fetcher/Main.java
@@ -13,15 +13,12 @@ public class Main {
 
     public static void main(String... args) throws Exception {
 
-        String endpoint = System.getenv().getOrDefault("TIMEZONEDB_ENDPOINT", "vip.timezonedb.com");
-        String key = System.getenv().getOrDefault("TIMEZONEDB_KEY", "3IEQDGPTOLJY");
+        String endpoint = System.getenv().getOrDefault("TIMEZONE_DB_ENDPOINT", "vip.timezonedb.com");
+        String key = System.getenv().getOrDefault("TIMEZONE_DB_KEY", "3IEQDGPTOLJY");
 
         TimeZoneDbClient zoneDbClient = new TimeZoneDbClient(endpoint, key);
         RatpackServer.start(server -> server
-                .serverConfig(config -> {
-                    config.baseDir(new File("html").getAbsoluteFile());
-                })
-
+                .serverConfig(config -> config.baseDir(new File("html").getAbsoluteFile()))
                 .handlers(chain ->
                         chain.files(f -> f.dir("").indexFiles("index.html")).
                         get("time", ctx -> ctx.insert(new TimeHandler(zoneDbClient))))
