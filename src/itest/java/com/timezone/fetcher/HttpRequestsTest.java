20f1bdf93d1e8b94d3930b1b2edce38ae8a219d3
denis256 <denis.o@linux.com>
2022-11-06 21:31:35 +0200

    Add integration tests

--- src/itest/java/com/timezone/fetcher/HttpRequestsTest.java
+++ src/itest/java/com/timezone/fetcher/HttpRequestsTest.java
@@ -1,6 +1,8 @@
 package com.timezone.fetcher;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.timezone.fetcher.handler.TimeHandlerResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,8 +10,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ratpack.core.http.client.ReceivedResponse;
 import ratpack.test.MainClassApplicationUnderTest;
 
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * End to End integration tests.
+ * Test -> Ratpack -> TImezoneDB
+ */
 @ExtendWith(MockitoExtension.class)
 public class HttpRequestsTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     MainClassApplicationUnderTest appUnderTest = new MainClassApplicationUnderTest(Main.class);
 
@@ -17,10 +30,83 @@ public class HttpRequestsTest {
     public void validRequest() throws Exception {
 
         appUnderTest.test(testHttpClient -> {
-            ReceivedResponse response = testHttpClient.get("/time");
+            ReceivedResponse response = testHttpClient.get(buildUri("RO", "Bucharest"));
+
+            TimeHandlerResponse parsedResponse = objectMapper.readValue(response.getBody().getBytes(), TimeHandlerResponse.class);
+            assertThat(parsedResponse).isNotNull();
+            assertThat(parsedResponse.getStatus()).isEqualTo(TimeHandlerResponse.Status.OK);
+            assertThat(parsedResponse.getPayload().size()).isEqualTo(2);
         });
 
     }
+
+    @Test
+    public void multipleMatches() throws Exception {
+
+        appUnderTest.test(testHttpClient -> {
+            ReceivedResponse response = testHttpClient.get(buildUri("US", "Timis*"));
+
+            TimeHandlerResponse parsedResponse = objectMapper.readValue(response.getBody().getBytes(), TimeHandlerResponse.class);
+            assertThat(parsedResponse).isNotNull();
+            assertThat(parsedResponse.getStatus()).isEqualTo(TimeHandlerResponse.Status.OK);
+            assertThat(parsedResponse.getPayload().size()).isEqualTo(4);
+        });
+
+    }
+
+    @Test
+    public void sameCityMultipleTimes() throws Exception {
+
+        appUnderTest.test(testHttpClient -> {
+            ReceivedResponse response = testHttpClient.get(buildUri("US", "Nashville"));
+
+            TimeHandlerResponse parsedResponse = objectMapper.readValue(response.getBody().getBytes(), TimeHandlerResponse.class);
+            assertThat(parsedResponse).isNotNull();
+            assertThat(parsedResponse.getStatus()).isEqualTo(TimeHandlerResponse.Status.OK);
+            assertThat(parsedResponse.getPayload().size()).isEqualTo(8);
+        });
+
+    }
+
+    @Test
+    public void noCity() throws Exception {
+
+        appUnderTest.test(testHttpClient -> {
+            ReceivedResponse response = testHttpClient.get(buildUri("RO", ""));
+
+            TimeHandlerResponse parsedResponse = objectMapper.readValue(response.getBody().getBytes(), TimeHandlerResponse.class);
+            assertThat(parsedResponse).isNotNull();
+            assertThat(parsedResponse.getStatus()).isEqualTo(TimeHandlerResponse.Status.ERROR);
+            assertThat(parsedResponse.getError()).contains("Missing city field");
+        });
+    }
+
+    @Test
+    public void noCityMatch() throws Exception {
+
+        appUnderTest.test(testHttpClient -> {
+            ReceivedResponse response = testHttpClient.get(buildUri("RO", "TestQwe"));
+
+            TimeHandlerResponse parsedResponse = objectMapper.readValue(response.getBody().getBytes(), TimeHandlerResponse.class);
+            assertThat(parsedResponse).isNotNull();
+            assertThat(parsedResponse.getStatus()).isEqualTo(TimeHandlerResponse.Status.ERROR);
+            assertThat(parsedResponse.getError()).contains("Record not found");
+        });
+    }
+
+    @Test
+    public void shortWildcard() throws Exception {
+
+        appUnderTest.test(testHttpClient -> {
+            ReceivedResponse response = testHttpClient.get(buildUri("RO", "Te*"));
+
+            TimeHandlerResponse parsedResponse = objectMapper.readValue(response.getBody().getBytes(), TimeHandlerResponse.class);
+            assertThat(parsedResponse).isNotNull();
+            assertThat(parsedResponse.getStatus()).isEqualTo(TimeHandlerResponse.Status.ERROR);
+            assertThat(parsedResponse.getError()).contains("Minimum 5 characters needed for wildcard search");
+        });
+    }
+
 
     @AfterEach
     public void shutdown() {
@@ -28,7 +114,7 @@ public class HttpRequestsTest {
     }
 
     private String buildUri(String country, String city) {
-        return String.format("/time?country=%s&city=%s", country, city);
+        return String.format("/time?country=%s&city=%s", URLEncoder.encode(country, Charset.defaultCharset()), URLEncoder.encode(city, Charset.defaultCharset()));
     }
 
 }
--- src/main/java/com/timezone/fetcher/Main.java
+++ src/main/java/com/timezone/fetcher/Main.java
@@ -9,12 +9,12 @@ import java.io.File;
 public class Main {
 
     public static void main(String... args) throws Exception {
-
         TimeZoneDbClient zoneDbClient = new TimeZoneDbClient("vip.timezonedb.com", "3IEQDGPTOLJY");
         RatpackServer.start(server -> server
                 .serverConfig(config -> {
                     config.baseDir(new File("html").getAbsoluteFile());
                 })
+
                 .handlers(chain ->
                         chain.files(f -> f.dir("").indexFiles("index.html")).
                         get("time", ctx -> ctx.insert(new TimeHandler(zoneDbClient))))
