1fec0997f48a0fee4131336b2b56f01827b0401a
denis256 <denis.o@linux.com>
2022-11-06 18:17:19 +0200

    Add handling of error response

--- src/test/java/com/timezone/fetcher/service/TimeZoneDbClientTest.java
+++ src/test/java/com/timezone/fetcher/service/TimeZoneDbClientTest.java
@@ -22,13 +22,10 @@ public class TimeZoneDbClientTest {
 
     @Test
     public void responseDeserialization() throws Exception {
-
         when(receivedResponse.getBody()).thenReturn(typedData);
-
         when(typedData.getBytes()).thenReturn(
                 IOUtils.toByteArray(this.getClass().getClassLoader().getResourceAsStream("timezonedb.json"))
         );
-
         TimeZoneDbClient timeZoneDbClient = new TimeZoneDbClient("test");
         TimeZoneDbResponse response = timeZoneDbClient.parseTimezone(receivedResponse);
 
@@ -39,7 +36,20 @@ public class TimeZoneDbClientTest {
         TimeZoneDbResponse.TimeZones timeZones = response.getZones().iterator().next();
         assertThat(timeZones.getFormatted()).isEqualTo("2022-11-06 18:13:35");
         assertThat(timeZones.getZoneName()).isEqualTo("Europe/Bucharest");
+    }
 
+    @Test
+    public void errorHandling() throws Exception {
+        when(receivedResponse.getBody()).thenReturn(typedData);
+        when(typedData.getBytes()).thenReturn(
+                IOUtils.toByteArray(this.getClass().getClassLoader().getResourceAsStream("timezonedb-error.json"))
+        );
+        TimeZoneDbClient timeZoneDbClient = new TimeZoneDbClient("test");
+        TimeZoneDbResponse response = timeZoneDbClient.parseTimezone(receivedResponse);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getStatus()).isEqualTo("FAILED");
+        assertThat(response.getZones().size()).isEqualTo(0);
     }
 
 }
